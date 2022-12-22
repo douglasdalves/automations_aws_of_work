965365d4186ad167f60164b7330ded4ada9f1f1b
Douglas_NotDell <douglas.santos106@yahoo.com>
2022-12-22 13:37:15 -0300

    conta ecam tf automacao

---
+++ aws-contas/ecam-partner-led/main.tf
@@ -0,0 +1,46 @@
+terraform {
+  required_version = ">=1.2.2"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.17.1"
+    }
+    template = {
+      source  = "hashicorp/template"
+      version = "2.2.0"
+    }
+  }
+}
+
+provider "aws" {
+  region  = var.aws_region
+  profile = var.aws_profile
+}
+
+locals {
+  now         = timestamp()
+  brasilia_tz = timeadd(local.now, "-3h")
+  date_br     = formatdate("DD/MM/YYYY", local.brasilia_tz)
+}
+
+locals {
+  common_tags = {
+    CreatedDate = local.brasilia_tz
+  }
+}
+
+## Chamada de Modulos ##
+#
+module "role_crossAccount" {
+  source            = "../../module-iam_role"
+  cross_account_arn = var.id_account_cross
+}
+
+module "trails-s3-logs" {
+  source = "../../module-trails-s3-logs"
+}
+
+module "grupo_admin" {
+  source = "../../module-iam_group"
+}
---
+++ aws-contas/ecam-partner-led/variables.tf
@@ -0,0 +1,32 @@
+variable "aws_region" {
+  type        = string
+  description = "Preecha a região a ser usada."
+  #default     = "Digite o seu"
+}
+
+#-----------------------------------------------------
+variable "aws_profile" {
+  type        = string
+  description = "Informar o profile configurado no aws cli"
+  #default     = "Digite o seu"
+}
+
+variable "registro" {
+  type        = map(string)
+  description = "Tags do terraform para contas."
+
+  default = {
+    "Owner"       = "Compass-cloud-n1"
+    "Environment" = "Conta-SPAM-Partnet-led"
+    "ManagedBy"   = "Terraform"
+  }
+}
+
+variable "id_account_cross" {
+  type        = number
+  description = "Account ID da role de CrossAccount"
+  default     = "766581111222"
+
+  #   default - payer-geral  = 471784020190
+  #   default - payer-interna = 766581111222
+}
